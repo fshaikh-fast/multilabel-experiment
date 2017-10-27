d6a741da0c27d6e79c2b4773825c328c30ee7f58
fshaikh <fshaikh@primaticsfinancial.com>
2017-10-27 11:13:30 -0400

    Adds new datasets for Experiment.

--- src/main/java/ExperimentExecuter.java
+++ src/main/java/ExperimentExecuter.java
@@ -4,7 +4,16 @@
 public class ExperimentExecuter {
     public static void main(String[] args) {
         // Call the main() method of MyClass
-        String[] datasets = new String[] {"src/main/data/Music.arff"};
+        String[] datasets = new String[] {
+                "src/main/data/bibtex/bibtex.arff",
+                "src/main/data/scene/scene.arff",
+                "src/main/data/yeast/yeast.arff",
+                "src/main/data/emotions/emotions.arff",
+                "src/main/data/CAL500/CAL500.arff",
+                "src/main/data/enron/enron.arff",
+                "src/main/data/flags/flags.arff",
+                "src/main/data/medical/medical.arff"
+        };
 
         try {
             ExperimentExample.main(datasets);
